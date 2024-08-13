ca8b77cf0c76f6a69543a2c6f6f8b3d3603990d9
Bruno Sá <iarlodsa@gmail.com>
2024-08-13 14:01:38 -0300

    feat(usercolumn): add some columns to users table and query mutation
    Create some columns, including an action dropdown menu

--- src/pages/userList.tsx
+++ src/pages/userList.tsx
@@ -1,13 +1,18 @@
 import { DataTable } from '@components/DataTable';
+import i18n from '@config/i18n/main';
 import { countTable } from '@src/database/queries/count';
+import { deleteFromTable } from '@src/database/queries/delete';
 import { selectManyUser } from '@src/database/queries/select';
-import { keepPreviousData, useQueries } from '@tanstack/react-query';
+import { keepPreviousData, useMutation, useQueries, useQueryClient } from '@tanstack/react-query';
 import { PaginationState } from '@tanstack/react-table';
 import { asyncWrapper } from '@utils/asyncWrapper';
+import { toastError, toastSuccess } from '@utils/toastMessage';
 import { columns } from '@utils/userColumn';
 import { useState } from 'react';
 
 const UserList = () => {
+  const queryClient = useQueryClient();
+
   const [pagination, setPagination] = useState<PaginationState>({
     pageIndex: 0,
     pageSize: 10,
@@ -39,10 +44,22 @@ const UserList = () => {
     ],
   });
 
+  const removeUser = useMutation({
+    mutationFn: async (id: number) => {
+      const [, error] = await asyncWrapper(deleteFromTable, 'users', id);
+      if (error) return toastError();
+      return toastSuccess(i18n.t('general.response.deleted', { what: i18n.t('general.data.user') }));
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['userList'] });
+      queryClient.invalidateQueries({ queryKey: ['userCount'] });
+    },
+  });
+
   return (
     <div>
       <DataTable
-        columns={columns}
+        columns={columns(removeUser)}
         data={userList.data ?? []}
         rowCount={userCount.data?.rowCount ?? 0}
         onPaginationChange={setPagination}
