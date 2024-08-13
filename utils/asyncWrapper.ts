3454604aa8b10039c04338cfd6a6c483c1b16c51
Bruno Sá <iarlodsa@gmail.com>
2024-08-13 13:45:40 -0300

    refactor(asyncwrapper): implements function overloading to handle `selectFromTableBy` function

--- utils/asyncWrapper.ts
+++ utils/asyncWrapper.ts
@@ -1,11 +1,24 @@
-export const asyncWrapper = async <T extends (...params: never[]) => unknown, U = Awaited<ReturnType<T>>>(
+import { Tables } from '@src/database/main';
+
+export function asyncWrapper<T extends keyof Tables, U extends Tables[T], V extends keyof U>(
+  fn: (table: T, key: V, value: U[V]) => Promise<U[]>,
+  table: T,
+  key: V,
+  value: U[V]
+): Promise<[U[] | null, unknown]>;
+export async function asyncWrapper<T extends (...params: never[]) => unknown, U = Awaited<ReturnType<T>>>(
   fn: T,
   ...args: Parameters<T>
-): Promise<[U | null, unknown]> => {
+): Promise<[U | null, unknown]>;
+
+export async function asyncWrapper<T extends (...params: never[]) => unknown, U = Awaited<ReturnType<T>>>(
+  fn: T,
+  ...args: Parameters<T>
+): Promise<[U | null, unknown]> {
   try {
     const data = (await fn(...args)) as U;
     return [data, null];
   } catch (err) {
     return [null, err];
   }
-};
+}
