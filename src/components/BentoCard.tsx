0570cde8d9da3ac062e9b02ef8590894f9a5fc4a
Bruno Sá <iarlodsa@gmail.com>
2024-08-15 22:02:37 -0300

    style(calendar): changes calendar and datepicker styles

--- src/components/BentoCard.tsx
+++ src/components/BentoCard.tsx
@@ -16,7 +16,7 @@ function BentoCardComponent({ background, className, desc, icon: Icon, title }: 
     <div
       className={cn(
         'group relative col-span-3 flex flex-col justify-between overflow-hidden rounded-xl',
-        'bg-card [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]',
+        'bg-card font-sans',
         'transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]',
         className
       )}
